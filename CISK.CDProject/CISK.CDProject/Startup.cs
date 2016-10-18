80c770fb0a5889bddb94a8decf28e0b252fe0022
kimtz <kim.tengbom-zetterman@ffcg.se>
2016-10-18 13:08:49 +0200

    Added project

---
+++ CISK.CDProject/CISK.CDProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CISK.CDProject.Startup))]
+namespace CISK.CDProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
