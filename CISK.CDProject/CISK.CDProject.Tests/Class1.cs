38e1a2187a6c09eac9abd50c06b9db0c34b2076f
kimtz <kim.tengbom-zetterman@ffcg.se>
2016-10-20 16:44:59 +0200

    Changed name and added Core and Tests

---
+++ CISK.CDProject/CISK.CDProject.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CISK.CDProject.Tests
+{
+    public class Class1
+    {
+    }
+}
