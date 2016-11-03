917c7c1d23550482353cecf5dd3ff1e3efbd62c9
kimtz <kim.tengbom-zetterman@ffcg.se>
2016-11-03 16:54:49 +0100

    more structure changes

--- CISK.CDProject/CISK.CDProject.Web/Properties/AssemblyInfo.cs
+++ CISK.CDProject/CISK.CDProject.Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9ec9a28e-b241-422c-8d7d-33868a26b6b8")]
+[assembly: Guid("97bed4ad-9001-4fee-ab82-326cebd03221")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
