9dd60fff585da9b8695b012079587a513b7eb38c
kimtz <kim.tengbom-zetterman@ffcg.se>
2016-12-01 15:55:54 +0100

    continued working on test API

--- CISK.CDProject/CISK.CDProject.API/Controllers/TestChildController.cs
+++ CISK.CDProject/CISK.CDProject.API/Controllers/TestChildController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CISK.CDProject.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CISK.CDProject.API.Controllers
@@ -110,6 +111,78 @@ namespace CISK.CDProject.API.Controllers
 
             testChildToUpdate.Name = testChild.Name;
             testChildToUpdate.Description = testChild.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("partiallyUpdateChild/{testClassId}/child/{childId}")]
+        public IActionResult PartiallyUpdateTestChild(int testClassId, int childId, [FromBody] JsonPatchDocument<TestChildForUpdate> patchDocument)
+        {
+            if (patchDocument == null)
+            {
+                return BadRequest();
+            }
+
+            var testClass = DummyData.Current.testList.FirstOrDefault(t => t.Id == testClassId);
+            if (testClass == null)
+            {
+                return NotFound();
+            }
+
+            var testChildToUpdate = testClass.TestChilds.FirstOrDefault(c => c.Id == childId);
+            if (testChildToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            var testChildToPatch = new TestChildForUpdate()
+            {
+                Description = testChildToUpdate.Description,
+                Name = testChildToUpdate.Name
+            };
+
+            patchDocument.ApplyTo(testChildToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (testChildToPatch.Name == testChildToPatch.Description)
+            {
+                ModelState.AddModelError("Description", "Description can't be the same as name");
+            }
+
+            TryValidateModel(testChildToPatch);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            testChildToUpdate.Name = testChildToPatch.Name;
+            testChildToUpdate.Description = testChildToPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("partiallyUpdateChild/{testClassId}/child/{childId}")]
+        public IActionResult DeleteTestChild(int testClassId, int childId)
+        {
+
+            var testClass = DummyData.Current.testList.FirstOrDefault(t => t.Id == testClassId);
+            if (testClass == null)
+            {
+                return NotFound();
+            }
+
+            var testChildToDelete = testClass.TestChilds.FirstOrDefault(c => c.Id == childId);
+            if (testChildToDelete == null)
+            {
+                return NotFound();
+            }
+
+            testClass.TestChilds.Remove(testChildToDelete);
 
             return NoContent();
         }
